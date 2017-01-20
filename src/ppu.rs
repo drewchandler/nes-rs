921a07e380fedd660244435c6051bbdede99e9f5
Drew Chandler <andrewschandler@gmail.com>
2017-01-19 21:18:34 -0800

    🗑 Sloppily implement background rendering

--- src/ppu.rs
+++ src/ppu.rs
@@ -1,7 +1,20 @@
 pub const CTRL_INCR_FLAG: u8 = 0x02;
+const CTRL_BACKGROUND_FLAG: u8 = 0x10;
 const CTRL_NMI_FLAG: u8 = 0x80;
+const MASK_DISPLAY_BACKGROUND: u8 = 0x08;
+const MASK_DISPLAY_SPRITES: u8 = 0x10;
 const STATUS_VBLANK_FLAG: u8 = 0x80;
 const PIXELS: usize = 256 * 240;
+
+static SYSTEM_PALETTE: [u32; 64] =
+    [0x757575, 0x271b8f, 0x0000ab, 0x47009f, 0x8f0077, 0xab0013, 0xa70000, 0x7f0b00, 0x432f00,
+     0x004700, 0x005100, 0x003f17, 0x1b3f5f, 0x000000, 0x000000, 0x000000, 0xbcbcbc, 0x0073ef,
+     0x233bef, 0x8300f3, 0xbf00bf, 0xe7005b, 0xdb2b00, 0xcb4f0f, 0x8b7300, 0x009700, 0x00ab00,
+     0x00933b, 0x00838b, 0x000000, 0x000000, 0x000000, 0xffffff, 0x3fbfff, 0x5f97ff, 0xa78bfd,
+     0xf77bff, 0xff77b7, 0xff7763, 0xff9b3b, 0xf3bf3f, 0x83d313, 0x4fdf4b, 0x58f898, 0x00ebdb,
+     0x000000, 0x000000, 0x000000, 0xffffff, 0xabe7ff, 0xc7d7ff, 0xd7cbff, 0xffc7ff, 0xffc7db,
+     0xffbfb3, 0xffdbab, 0xffe7a3, 0xe3ffa3, 0xabf3bf, 0xb3ffcf, 0x9ffff3, 0x000000, 0x000000,
+     0x000000];
 
 pub struct CycleResult {
     pub end_frame: bool,
@@ -29,6 +42,11 @@ pub struct Ppu {
     vram: [u8; 16384],
     spr_ram: [u8; 256],
     pub screen: [u32; PIXELS],
+    name_table_byte: u8,
+    attribute_table_byte: u8,
+    low_bg_tile_byte: u8,
+    high_bg_tile_byte: u8,
+    tile_data: u64,
     cycle: u16,
     scanline: i16,
 }
@@ -47,6 +65,11 @@ impl Ppu {
             vram: [0; 16384],
             spr_ram: [0; 256],
             screen: [0; PIXELS],
+            name_table_byte: 0,
+            attribute_table_byte: 0,
+            low_bg_tile_byte: 0,
+            high_bg_tile_byte: 0,
+            tile_data: 0,
             cycle: 0,
             scanline: -1,
         }
@@ -104,7 +127,12 @@ impl Ppu {
     }
 
     pub fn write_vram_data(&mut self, value: u8) {
-        self.vram[self.vram_addr as usize] = value;
+        let addr = self.vram_addr;
+        self.write_vram(addr, value);
+        self.incr_vram_addr();
+    }
+
+    fn incr_vram_addr(&mut self) {
         let incr = self.addr_increment();
         self.vram_addr += incr;
     }
@@ -113,6 +141,27 @@ impl Ppu {
         let mut nmi = false;
 
         match self.scanline {
+            -1 => {
+                if self.rendering_enabled() {
+                    self.fetch_data();
+
+                    if self.cycle >= 280 && self.cycle <= 304 {
+                        self.vram_addr = (self.vram_addr & 0x841F) | (self.tmp_vram_addr & 0x7BE0);
+                    }
+                }
+            }
+            0...239 => {
+                if self.rendering_enabled() {
+                    if self.cycle >= 1 && self.cycle <= 256 {
+                        self.render_pixel();
+                    }
+                    self.fetch_data();
+
+                    if self.cycle == 257 {
+                        self.vram_addr = (self.vram_addr & 0xFBE0) | (self.tmp_vram_addr & 0x041F);
+                    }
+                }
+            }
             241 => {
                 if self.cycle == 1 {
                     self.set_vblank(true);
@@ -125,12 +174,124 @@ impl Ppu {
                 if self.cycle == 1 {
                     self.set_vblank(false);
                 }
+                if self.rendering_enabled() {
+                    self.fetch_data();
+
+                    if self.cycle >= 280 && self.cycle <= 304 {
+                        self.vram_addr = (self.vram_addr & 0x841F) | (self.tmp_vram_addr & 0x7BE0);
+                    }
+                }
             }
             _ => {}
         }
 
         let end_frame = self.tick();
         CycleResult::new(end_frame, nmi)
+    }
+
+    fn rendering_enabled(&self) -> bool {
+        self.display_background() || self.display_sprites()
+    }
+
+    fn render_pixel(&mut self) {
+        let shift = 32 + (7 - self.scroll) * 4;
+        let bg_color_addr = (0x3f00 | self.tile_data >> shift) as u16;
+        let bg_palette_index = self.read_vram(bg_color_addr);
+        let color = SYSTEM_PALETTE[(bg_palette_index % 64) as usize];
+        self.screen[256 * self.scanline as usize + self.cycle as usize - 1] = color;
+    }
+
+    fn fetch_data(&mut self) {
+        match self.cycle {
+            1...256 | 321...336 => self.fetch_bg_data(),
+            257 => self.increment_y(),
+            320 => {}
+            _ => {}
+        }
+    }
+
+    fn fetch_bg_data(&mut self) {
+        self.tile_data <<= 4;
+
+        match self.cycle % 8 {
+            1 => self.fetch_name_table_byte(),
+            3 => self.fetch_attribute_table_byte(),
+            5 => self.fetch_low_bg_tile_byte(),
+            7 => self.fetch_high_bg_tile_byte(),
+            0 => {
+                self.push_tile_data();
+                self.increment_x();
+            }
+            _ => {}
+        }
+    }
+
+    fn fetch_name_table_byte(&mut self) {
+        let addr = 0x2000 | self.vram_addr & 0x0fff;
+        self.name_table_byte = self.read_vram(addr);
+    }
+
+    fn fetch_attribute_table_byte(&mut self) {
+        let addr = 0x23C0 | (self.vram_addr & 0x0C00) | ((self.vram_addr >> 4) & 0x38) |
+                   ((self.vram_addr >> 2) & 0x07);
+        let shift = ((self.vram_addr >> 4) & 0x04) | (self.vram_addr & 0x02);
+        self.attribute_table_byte = ((self.read_vram(addr) >> shift) & 0x03) << 2;
+    }
+
+    fn fetch_low_bg_tile_byte(&mut self) {
+        let fine_y = (self.vram_addr >> 12) & 0x07;
+        let tile = self.name_table_byte as u16;
+        let addr = self.background_pattern_table() + tile * 16 + fine_y;
+        self.low_bg_tile_byte = self.read_vram(addr);
+    }
+
+    fn fetch_high_bg_tile_byte(&mut self) {
+        let fine_y = (self.vram_addr >> 12) & 0x07;
+        let tile = self.name_table_byte as u16;
+        let addr = self.background_pattern_table() + tile * 16 + fine_y + 8;
+        self.high_bg_tile_byte = self.read_vram(addr);
+    }
+
+    fn push_tile_data(&mut self) {
+        let mut new_tile_data = 0u64;
+
+        for _ in 0..8 {
+            let low_byte = (self.low_bg_tile_byte & 0x80) >> 7;
+            let high_byte = (self.high_bg_tile_byte & 0x80) >> 6;
+
+            self.low_bg_tile_byte <<= 1;
+            self.high_bg_tile_byte <<= 1;
+            new_tile_data = (new_tile_data << 4) |
+                            (self.attribute_table_byte | high_byte | low_byte) as u64;
+        }
+
+        self.tile_data = self.tile_data | new_tile_data;
+    }
+
+    fn increment_x(&mut self) {
+        if (self.vram_addr & 0x001F) == 31 {
+            self.vram_addr = self.vram_addr & !0x001F ^ 0x0400;
+        } else {
+            self.vram_addr += 1;
+        }
+    }
+
+    fn increment_y(&mut self) {
+        if (self.vram_addr & 0x7000) != 0x7000 {
+            self.vram_addr += 0x1000;
+        } else {
+            self.vram_addr &= !0x7000;
+            let mut y = (self.vram_addr & 0x03E0) >> 5;
+            if y == 29 {
+                y = 0;
+                self.vram_addr ^= 0x0800;
+            } else if y == 31 {
+                y = 0;
+            } else {
+                y += 1;
+            }
+            self.vram_addr = (self.vram_addr & !0x03E0) | (y << 5);
+        }
     }
 
     fn tick(&mut self) -> bool {
@@ -159,8 +320,24 @@ impl Ppu {
         }
     }
 
+    fn background_pattern_table(&self) -> u16 {
+        if self.ctrl & CTRL_BACKGROUND_FLAG == 0 {
+            0
+        } else {
+            0x1000
+        }
+    }
+
     fn nmi_flag(&self) -> bool {
         self.ctrl & CTRL_NMI_FLAG != 0
+    }
+
+    fn display_background(&self) -> bool {
+        self.mask & MASK_DISPLAY_BACKGROUND != 0
+    }
+
+    fn display_sprites(&self) -> bool {
+        self.mask & MASK_DISPLAY_SPRITES != 0
     }
 
     fn set_vblank(&mut self, value: bool) {
@@ -169,6 +346,24 @@ impl Ppu {
         } else {
             self.status & !STATUS_VBLANK_FLAG
         };
+    }
+
+    fn read_vram(&self, addr: u16) -> u8 {
+        self.vram[self.map_addr(addr)]
+    }
+
+    fn write_vram(&mut self, addr: u16, value: u8) {
+        self.vram[self.map_addr(addr)] = value;
+    }
+
+    fn map_addr(&self, addr: u16) -> usize {
+        match addr {
+            0...0x2fff | 0x3f00...0x3f1f => addr as usize,
+            0x3000...0x3eff => addr as usize - 0x1000,
+            0x3f20...0x3fff => ((addr - 0x3f20) % 32 + 0x3f00) as usize,
+            0x4000...0xffff => self.map_addr(addr % 0x4000),
+            _ => panic!("UNIMPLEMENTED ADDR {:x}", addr),
+        }
     }
 }
 
